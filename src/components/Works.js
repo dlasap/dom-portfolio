21f70e97ccf2e0ea7b3fda2bae0f28eb6535286f
dom <lasapdominic@gmail.com>
2023-03-02 18:10:21 +0800

    updated work page

--- src/components/Works.js
+++ src/components/Works.js
@@ -19,7 +19,9 @@ const Works = () => {
                 <li className="tech-item">NodeJS</li>
                 <li className="tech-item">TypeScript</li>
                 <li className="tech-item">MongoDB</li>
-                which were React, TypeScript, NodeJS
+                <li className="tech-item">ExpressJS</li>
+                <li className="tech-item">HTML</li>
+                <li className="tech-item">CSS</li>
               </div>
             </span>
             <p className="work-desc">
@@ -34,9 +36,9 @@ const Works = () => {
               <p>Condominium Rentals Page for Viewing</p>
               <div className="tech-stack">
                 <li className="tech-item">ReactJS</li>
-                <li className="tech-item">NodeJS</li>
                 <li className="tech-item">TypeScript</li>
-                <li className="tech-item">MongoDB</li>
+                <li className="tech-item">HTML</li>
+                <li className="tech-item">CSS</li>
               </div>
             </span>
             <p className="work-desc">
@@ -49,17 +51,19 @@ const Works = () => {
             <span className="work-title">
               <p>ReSynchronization for BackEnd Service</p>
               <div className="tech-stack">
-                <li className="tech-item">ReactJS</li>
                 <li className="tech-item">NodeJS</li>
                 <li className="tech-item">TypeScript</li>
-                <li className="tech-item">MongoDB</li>
-                NodeJS, NestJS , WebHooks, and the databases mentioned were used primarily.
+                <li className="tech-item">NestJS</li>
+                <li className="tech-item">WebHooks</li>
+                <li className="tech-item">ReThinkDB</li>
+                <li className="tech-item">Redis</li>
+                <li className="tech-item">ElasticSearch</li>
               </div>
             </span>
             <p className="work-desc">
-              Basically the Company's API has been using 3 different Databases: Redis, RethinkDB and ElasticSearch. It was my task to observe the data
-              inaccuracies and as well as fix/sychronize data that aren't synced and accurate. All databases should be synced and monitored by the
-              DevOps team after deployment and notifications were sent on an internal channel for information about the health of the database.
+              Since the data of our API is buggy/out of sync sometimes. It was my task to observe the data inaccuracies and as well as fix/sychronize
+              data that aren't synced and accurate. All databases should be synced and monitored by the DevOps team after deployment and notifications
+              were sent on an internal channel for information about the health of the database.
             </p>
           </li>
           <li className="work-item">
@@ -67,10 +71,11 @@ const Works = () => {
             <span className="work-title">
               <p>E-mail Service</p>
               <div className="tech-stack">
-                <li className="tech-item">ReactJS</li>
+                <li className="tech-item">NestJS</li>
                 <li className="tech-item">NodeJS</li>
                 <li className="tech-item">TypeScript</li>
-                <li className="tech-item">MongoDB</li>
+                <li className="tech-item">ElasticSearch</li>
+                <li className="tech-item">SendGrid API</li>
               </div>
             </span>
             <p className="work-desc">
@@ -86,14 +91,13 @@ const Works = () => {
                 <li className="tech-item">ReactJS</li>
                 <li className="tech-item">NodeJS</li>
                 <li className="tech-item">TypeScript</li>
-                <li className="tech-item">MongoDB</li>
-                and its tech stack is on JavaScript: React, NodeJS, TypeScript, NestJS.
+                <li className="tech-item">NestJS</li>
               </div>
             </span>
             <p className="work-desc">
-              It was our primary client in our company. I worked with the frontend side for 6 months and was transferred to the backend due to my
-              evaluation and skill assessments. I was more fit on the backend than in the frontend, lack of man power in the said part was also the
-              reason for transfer. I enjoyed both but I loved working at the backend more.
+              It was a vehicles rental business web app. I worked with the frontend side for 6 months and was transferred to the backend due to my
+              evaluation and skill assessments. I was more fit on the backend than in the frontend. I enjoyed both but I loved working at the backend
+              more.
             </p>
           </li>
           <li className="work-item">
@@ -101,17 +105,17 @@ const Works = () => {
             <span className="work-title">
               <p>PostgresSQL CRUD ORM & Normalizer</p>
               <div className="tech-stack">
-                <li className="tech-item">ReactJS</li>
                 <li className="tech-item">NodeJS</li>
+                <li className="tech-item">NestJS</li>
                 <li className="tech-item">TypeScript</li>
-                <li className="tech-item">MongoDB</li>
-                Postgresql, NodeJS, NestJS,TypeScript
+                <li className="tech-item">Postgresql</li>
               </div>
             </span>
             <p className="work-desc">
-              I was primarily tasked to make a Object related mapping or basically a normalizer for our existing API to adapt to our plan to be new
-              database which is postgres, just for a backup and maybe soon to be primary database since our Redis and elasticsearch sometimes have
-              trouble syncing. It was more of the backend side project assigned by my team lead.
+              {" "}
+              It was a backend side project assigned by my team lead which was to make an Object related mapping or basically a normalizer for our
+              existing API to be adapted to PostgresSQL, and maybe soon to be primary database since our Redis and elasticsearch are buggy/out of
+              sync.
             </p>
           </li>
         </ul>
